c1538bd1cf8fff3aa5c5ef2df8dcfc99f61e3300
toddteigland <toddteigland@gmail.com>
2022-11-24 20:25:32 -0800

    Added funciton to print to the console the next flyover times

---
+++ index.js
@@ -0,0 +1,34 @@
+const { fetchMyIP, fetchCoordsByIP, nextISSTimesForMyLocation , fetchISSFlyOverTimes } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
+//   console.log(ip);
+// });
+
+// fetchCoordsByIP((ip) => {
+//   if(error) {
+//     console.log(error);
+//   }
+//   console.log(data);
+// });
+
+
+const printPassTimes = function(times) {
+  for (const pass of times) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = times.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((err, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    printPassTimes(body);
+  }
+});
